f14d4af15096010a266d7c0dc0a2461016d25af1
Adam Ledwon <ledwon.adam1989@gmail.com>
2022-12-15 13:41:03 +0100

    improve js code

--- js/button__function.js
+++ js/button__function.js
@@ -1,12 +1,23 @@
-let button = document.querySelector(".js-button");
-let body = document.querySelector(".body__light");
-let themeName = document.querySelector(".js-buttonFunction");
-
-button.addEventListener("click", () => {
-    body.classList.toggle("body__dark");
-    if (body.classList.contains("body__dark")) {
-        button__functionName.innerText = "jasny";
-    } else {
-        button__functionName.innerText = "ciemny";
+{
+    const welcome = () => {
+        console.log("Witam wszystkich na stronie poświęconej moim zainteresowaniom");
     }
-});
+    
+    const backgroundToogle = () => {
+        let body = document.querySelector(".body__light");
+        body.classList.toggle("body__dark");
+        if (body.classList.contains("body__dark")) {
+        js-buttonFunction.innerText = "jasny";
+        } else {
+        js-buttonFunction.innerText = "ciemny";
+        }
+    };
+
+    const init = () => {
+        let button = document.querySelector(".js-button");
+        button.addEventListener("click", backgroundToogle);
+        welcome();
+    }
+
+    init();
+}
